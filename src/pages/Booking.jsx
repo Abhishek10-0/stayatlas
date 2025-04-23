af7a1a201f983f8b0da70bbed449ca385501ee74
Abhishek10-0 <abhishekchoudhary162004@gmail.com>
2025-04-23 14:08:18 +0530

    VilaDetail

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import Footer from '../componenets/footer'
+import Footer from '../componets/footer'
+import Header from '../componets/Header'
+import VillaDetails from '../componets/VilaDetail'
+
 
 const Booking = () => {
   return (
     <div>
+      <Header/>
+      <VillaDetails/>
       <Footer/>
     </div>
   )
