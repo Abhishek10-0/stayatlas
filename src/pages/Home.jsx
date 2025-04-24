b5f320c18f1e4aea352b68c13a36b4cfdd5e7c79
hershey984 <harshsawant984@gmail.com>
2025-04-24 11:21:44 +0530

    added slideshow to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Slideshow from '../components/Slideshow'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+     <div>Home</div>
+     <div>
+      <Slideshow></Slideshow>
+      
+     </div>
+    </>
   )
 }
 
